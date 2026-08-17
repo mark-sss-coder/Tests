da1df951990a23e244c02c8b041f8fe25026b8d0
mark-sss-coder <sss.marix@gmail.com>
2026-08-17 10:55:51 +0300

    Update Sandboxel 1.js

--- Sandboxel 1.js
+++ Sandboxel 1.js
@@ -1,31 +1,30 @@
-// 1. Правила Игры «Жизнь» Конвея (доступны глобально)
+// 1. Правила Игры «Жизнь» (доступны глобально)
 window.lifeRules = {
     live: { alive: [2, 3] },
     dead: { birth: [3] },
-    neighbors: [
-        [0, 1, 1, 0], [1, 0, 1, 0], [0, -1, 1, 0], [-1, 0, 1, 0],
-        [1, 1, 1, 0], [-1, 1, 1, 0], [-1, -1, 1, 0], [1, -1, 1, 0]
+    neighbors: [, [1, 0, 1, 0], [0, -1, 1, 0], [-1, 0, 1, 0],
+, [-1, 1, 1, 0], [-1, -1, 1, 0], [1, -1, 1, 0]
     ]
 };
 
-// 2. Описание элементов
+// 2. Блок-приветствие (самоочищается при клике)
 elements['Welcome to conway\'s game of life!'] = {
     color: '#c8c8c8',
     category: 'land',
     density: 1000,
-    tick(pixel) {
-        if (pixel && typeof pixel.x !== 'undefined') {
-            deletePixel(pixel.x, pixel.y); 
-        }
+    tick: function(pixel) {
+        pixel.element = "eraser"; // По официальному гайду: просто превращаем в ластик/пустоту
     }
 };
 
+// 3. Живая клетка
 elements.alive = {
     color: '#cecece',
     category: 'land',
     density: 1000,
-    tick(pixel) {
+    tick: function(pixel) {
         let neigh = 0;
+        
         for (const [xPos, yPos, weight, deadWeight] of window.lifeRules.neighbors) {
             let nx = pixel.x + xPos;
             let ny = pixel.y + yPos;
@@ -33,6 +32,7 @@ elements.alive = {
             if (outOfBounds(nx, ny)) {
                 neigh += deadWeight ?? 0;
             } else {
+                // Используем правильные оси из ядра Sandboxels: pixelMap[y][x]
                 let checkPixel = pixelMap[ny] ? pixelMap[ny][nx] : null;
                 if (checkPixel && checkPixel.element === 'alive') {
                     neigh += weight ?? 1;
@@ -41,18 +41,24 @@ elements.alive = {
                 }
             }
         }
+        
+        // Записываем будущее состояние в кастомное свойство объекта пикселя
         if (!window.lifeRules.live.alive.includes(neigh)) {
-            pixel.data.nextState = 'dead';
+            pixel.nextState = 'dead';
+        } else {
+            pixel.nextState = 'alive';
         }
     }
 };
 
+// 4. Мертвая клетка
 elements.dead = {
     color: '#989898',
     category: 'land',
     density: 1000,
-    tick(pixel) {
+    tick: function(pixel) {
         let neigh = 0;
+        
         for (const [xPos, yPos, weight, deadWeight] of window.lifeRules.neighbors) {
             let nx = pixel.x + xPos;
             let ny = pixel.y + yPos;
@@ -68,35 +74,32 @@ elements.dead = {
                 }
             }
         }
+        
         if (window.lifeRules.dead.birth.includes(neigh)) {
-            pixel.data.nextState = 'alive';
+            pixel.nextState = 'alive';
+        } else {
+            pixel.nextState = 'dead';
         }
     }
 };
 
-// 3. Вывод кнопок в начало первой вкладки "land"
+// 5. Чистый перенос состояния (Вызывается движком на каждом кадре для каждого пикселя)
+// Этот дополнительный шаг гарантирует синхронность без перезаписи ядра
+elements.alive.postTick = function(pixel) {
+    if (pixel.nextState) {
+        pixel.element = pixel.nextState;
+    }
+};
+elements.dead.postTick = elements.alive.postTick;
+
+// 6. Сортировка кнопок (выводим их в самый верх вкладки land)
 const oldElements = { ...elements };
 for (const key in elements) delete elements[key];
 elements.alive = oldElements.alive;
 elements.dead = oldElements.dead;
 Object.assign(elements, oldElements);
 
+// Обновляем интерфейс игры
 if (typeof window.createButtons === "function") {
     window.createButtons();
 }
-
-// 4. Патч главного цикла для честной смены поколений
-const originalUpdateSim = window.updateSim;
-window.updateSim = function() {
-    if (originalUpdateSim) originalUpdateSim();
-
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            let pixel = pixelMap[y] ? pixelMap[y][x] : null;
-            if (pixel && pixel.data && pixel.data.nextState) {
-                changePixel(pixel, pixel.data.nextState, true);
-                pixel.data.nextState = null;
-            }
-        }
-    }
-};
